50e36b3c277dc1ecba56336f3a9087161d219ab4
GladysDesign <gladys.design@outlook.com>
2025-03-20 19:55:13 +0000

    initial commit
    initial commit

--- app/routes.js
+++ app/routes.js
@@ -7,3 +7,22 @@ const govukPrototypeKit = require('govuk-prototype-kit')
 const router = govukPrototypeKit.requests.setupRouter()
 
 // Add your routes here
+router.post('/benefit-selection-answer', function (req, res) {
+
+    
+    var selection = req.session.data['whattypeofbenefit']
+  
+    // Check whether the variable matches a condition
+    if (selection == "low-income"){
+      // Send user to next page
+      res.redirect('benefits/low-income')
+    } else if (selection = 'disability'){
+        res.redirect('benefits/disability')
+    } else if (selection = 'families'){
+        res.redirect('benefits/families')
+    }else if (selection = 'other'){
+        res.redirect('benefits/otherbenefits')
+    }
+ 
+  
+  })
