095ff10703e8cb2e6828b24dd8e65ec35d6d1ddf
GladysDesign <gladys.design@outlook.com>
2025-04-07 11:49:56 +0100

    quick fixes + update links on right side of page

--- app/routes.js
+++ app/routes.js
@@ -7,10 +7,27 @@ const govukPrototypeKit = require('govuk-prototype-kit')
 const router = govukPrototypeKit.requests.setupRouter()
 
 // Add your routes here
+router.post('/passTrackingValidate', function (req, res) {
+
+    
+    var trackingNb = req.session.data['passport-trackingNb']
+    console.log("tracking: " + JSON.stringify(trackingNb));
+
+    // Check whether the variable matches a condition
+    if (trackingNb){
+        // Send user to next page
+        res.redirect('passport/passport-lastname')
+     } else {
+        console.log("error!!!!")
+    }
+ 
+  
+  })
+
 router.post('/benefit-selection-answer', function (req, res) {
 
     
-    var selection = req.session.data['whattypeofbenefit']
+    var selection = req.session.data['typeofbenefit']
   
     // Check whether the variable matches a condition
     if (selection == "low-income"){
@@ -22,6 +39,26 @@ router.post('/benefit-selection-answer', function (req, res) {
         res.redirect('benefit-claim/families')
     }else if (selection = 'other'){
         res.redirect('benefit-claim/otherbenefits')
+    }
+ 
+  
+  })
+
+  router.post('/benefitTracking', function (req, res) {
+  
+    var selection = req.session.data['benefit-trackingNb']
+
+    // Check whether the variable matches a condition
+    if (selection){
+      // Send user to next page
+      res.redirect('/benefit-claim/status')
+    
+    } else 
+    {
+        req.session.data['errorMessage'] = "This tracking number is invalid";
+        console.log("error " + JSON.stringify(req.session.data));
+        res.redirect('../benefit-claim/tracking-number');
+    
     }
  
   
